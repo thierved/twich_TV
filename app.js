fde40b1f6d6040dc3af56c4a3aac7fc267e34d33
Thierno Sadou Diallo <thierved01@gmail.com>
2018-02-15 18:04:22 +0000

    got the json file

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 
 const channels = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
 
-
-$.each(channels, function(i) {
-  $.getJSON('https://wind-bow.glitch.me/twitch-api/channels/'+channels[i]+'?callback=?', function(data) {
+let output = '';
+for(i of channels) {
+  $.getJSON('https://wind-bow.glitch.me/twitch-api/channels/'+i+'?callback=?', function(data) {
+      output += '<li>';
+      output += '<p>'+ data.name + '</p>' ;
+      output += '</li>';
       console.log(data.status);
+      console.log(output);
+      $('#channel').html(output);
     });
-});
+};
