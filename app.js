d2bfed4caf2328dd398d54d6803adf63a45935df
Thierno Sadou Diallo <thierved01@gmail.com>
2018-02-16 18:05:33 +0000

    finished designing the channels

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const channels = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbec
 let output = '';
 for(i of channels) {
   $.getJSON('https://wind-bow.glitch.me/twitch-api/channels/'+i+'?callback=?', function(data) {
-      output += '<li>';
+      output += '<li><span class="live">live</span>';
       output += '<div class=placeholder><img src='+data.logo+'></img></div>';
       output += '<div class="info"><a href='+data.url+'>'+ data.name + '</a>';
       if (data.status !== null) {
