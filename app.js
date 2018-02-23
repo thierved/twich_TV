1501354811eeb25aae9906d80b3d63cf277d1f5d
Thierno Sadou Diallo <thierved01@gmail.com>
2018-02-23 09:53:41 +0000

    add the event on the buttons

--- app.js
+++ app.js
@@ -2,11 +2,25 @@
 const channels = ["freecodecamp", "ESL_SC2", "OgamingSC2", "cretetion", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
 
 let output = '';
+let allBtn = $('#all'),
+    online = $('#online'),
+    offline = $('#offline');
+
 for(i of channels) {
   $.getJSON('https://wind-bow.glitch.me/twitch-api/channels/'+i+'?callback=?', function(data) {
-    viewChannels(data);
+      viewChannels(data);
     });
 };
+  let li;
+online.on('click', function() {
+  li = $('li').children().find('.offline').parent().parent().remove();
+});
+
+allBtn.on('click', function() {
+  if (li) {
+    li.appendTo($('#channel'));
+  }
+});
 
 
 function viewChannels(jsonData) {
@@ -16,7 +30,7 @@ function viewChannels(jsonData) {
   if (jsonData.status !== null) {
     output += '<p class="status">'+ jsonData.status+ '</p/>'
   } else {
-    output += '<p class="status">currently offline!</p/></div>'
+    output += '<p class="status offline">currently offline!</p/></div>'
   }
   output += '</li>';
   $('#channel').html(output);
